5bf50ae6f7c16a73bdb54e7fe69968f7d7460432
Aleena <aleenaros999@gmail.com>
2024-10-26 14:38:43 +0530

    verify token middleware

---
+++ backend/app/middlewares/verifyToken.ts
@@ -0,0 +1,50 @@
+import { User } from "app/models/User";
+import { ResponseHelper } from "app/utils/ResponseHelper";
+import { NextFunction, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import config from "config";
+import { TokenUtil } from "app/utils/TokenUtil";
+
+export const verifyToken = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const tokenSecret = config.get<string>("token.jwtTokenSecret");
+
+  try {
+    const token = req.cookies?.token;
+    if (!token) {
+      return ResponseHelper.handleError(
+        res,
+        "Missing token",
+        {},
+        StatusCodes.UNAUTHORIZED
+      );
+    }
+
+    const decodeToken = TokenUtil.verifyToken(token);
+    const user = await User.findOne({ userId: decodeToken.userId }).select(
+      "-password"
+    );
+
+    if (!user) {
+      return ResponseHelper.handleError(
+        res,
+        "Invalid access token",
+        {},
+        StatusCodes.UNAUTHORIZED
+      );
+    }
+
+    res.locals.user = user;
+    next();
+  } catch (error) {
+    return ResponseHelper.handleError(
+      res,
+      "Unauthorized",
+      {},
+      StatusCodes.UNAUTHORIZED
+    );
+  }
+};
