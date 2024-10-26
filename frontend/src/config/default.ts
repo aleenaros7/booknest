1e4d18031ad79ccf2024410646cebf5692a8d1a0
Aleena <aleenaros999@gmail.com>
2024-10-26 19:22:56 +0530

    config template

---
+++ frontend/src/config/default.ts
@@ -0,0 +1,10 @@
+export const config = {
+  environment: process.env.NODE_ENV || "development",
+  api: {
+    baseUrl: "http://localhost:5000",
+    user: {},
+    auth: {},
+    books: {},
+    librarian: {},
+  },
+};
