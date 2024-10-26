c9e933f5f889b9fe2a12b58ae9f83b46f22ab2be
Aleena <aleenaros999@gmail.com>
2024-10-26 14:42:04 +0530

    routes

--- backend/app/server.ts
+++ backend/app/server.ts
@@ -2,6 +2,9 @@ import cors from "cors";
 import express, { Request, Response, type Express } from "express";
 import config from "config";
 import { ResponseHelper } from "./utils/ResponseHelper";
+import { userRouter } from "./routers/userRoutes";
+import { authRouter } from "./routers/authRoutes";
+import { librarianRouter } from "./routers/librarianRoutes";
 
 const app: Express = express();
 
@@ -11,5 +14,9 @@ app.use(cors());
 app.get("/", (_req: Request, res: Response) => {
   ResponseHelper.handleSuccess(res, "healthy");
 });
+
+app.use("/auth", authRouter);
+app.use("/users", userRouter);
+app.use("/librarian", librarianRouter);
 
 export { app };
