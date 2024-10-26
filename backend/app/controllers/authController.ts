db9a98f28b733c4fa45b6fe7aedb95b30b783f58
Aleena <aleenaros999@gmail.com>
2024-10-26 14:41:14 +0530

    login route

--- backend/app/controllers/authController.ts
+++ backend/app/controllers/authController.ts
@@ -38,3 +38,37 @@ export const registerUser = async (req: Request, res: Response) => {
     return ResponseHelper.handleError(res, "Creating user failed");
   }
 };
+
+export const loginUser = async (req: Request, res: Response) => {
+  try {
+    const { userName, password } = req.body;
+    const user = await User.findOne({ userName });
+
+    if (!user) {
+      return ResponseHelper.handleError(
+        res,
+        "User not found",
+        {},
+        StatusCodes.NOT_FOUND
+      );
+    }
+
+    if (!(await PasswordUtil.verifyPassword(password, user.password))) {
+      return ResponseHelper.handleError(
+        res,
+        "Invalid credentials",
+        {},
+        StatusCodes.UNAUTHORIZED
+      );
+    }
+
+    const token = TokenUtil.generateToken(user);
+
+    ResponseHelper.handleSuccess(res, "Login successful", {}, StatusCodes.OK, [
+      { name: "token", value: token, options: tokenOptions },
+    ]);
+  } catch (error) {
+    console.log(error);
+    return ResponseHelper.handleError(res, "Login failed");
+  }
+};
--- backend/app/routers/authRoutes.ts
+++ backend/app/routers/authRoutes.ts
@@ -1,10 +1,11 @@
 import express, { type Router } from "express";
 import { validateRequest } from "app/middlewares/validator";
 import { loginUserSchema, registerUserSchema } from "app/schema";
-import { registerUser } from "app/controllers/authController";
+import { loginUser, registerUser } from "app/controllers/authController";
 
 const authRouter: Router = express.Router();
 
 authRouter.post("/signup", validateRequest(registerUserSchema), registerUser);
+authRouter.post("/login", validateRequest(loginUserSchema), loginUser);
 
 export { authRouter };
