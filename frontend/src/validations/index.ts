76034f5187aeae6bc655c8f4e2906f423ebd37e4
Aleena <aleenaros999@gmail.com>
2024-10-26 19:10:32 +0530

    signup schema

--- frontend/src/validations/index.ts
+++ frontend/src/validations/index.ts
@@ -1,10 +1,33 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-    username: z.string().trim().min(1, "username is required"),
-    password: z
-      .string()
+  userName: z.string().trim().min(1, "username is required"),
+  password: z
+    .string()
+    .trim()
+    .min(1, "password is required")
+    .min(6, "password must be at least 6 characters"),
+});
+
+export const signupSchema = z.object({
+  fullName: z
+      .string({ required_error: 'fullName is required' })
       .trim()
-      .min(1, "password is required")
-      .min(6, "password must be at least 6 characters"),
-  });
+      .min(3, { message: 'Name must be at least 3 characters' })
+      .max(100, { message: 'Name must be at most 100 characters' }),
+  userName: z
+      .string({ required_error: 'userName is required' })
+      .trim()
+      .min(3, { message: 'userName must be at least 3 characters' })
+      .max(30, { message: 'userName must be at most 100 characters' }),
+  email: z
+      .string({ required_error: 'email is required' })
+      .trim()
+      .email({ message: 'Invalid email address' })
+      .min(3, { message: 'email must be at least 3 characters' })
+      .max(100, { message: 'email must be at most 100 characters' }),
+  password: z
+      .string({ required_error: 'password is required' })
+      .min(8, { message: 'password must be at least 8 characters' })
+      .max(100, { message: 'password must be at most 100 characters' }),
+});
