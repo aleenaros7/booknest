cdb985ec76af33af0e45e741120d10bc2b4d97c8
Aleena <aleenaros999@gmail.com>
2024-10-26 19:12:18 +0530

    react query provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,29 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Error, Librarian, Login, Signup, Student } from "./pages";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/sign-up" element={<Signup />} />
-        <Route path="/librarian" element={<Librarian />} />
-        <Route path="/student" element={<Student />} />
-        <Route path="/" element={<Navigate to={"/login"} />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/librarian" element={<Librarian />} />
+          <Route path="/student" element={<Student />} />
+          <Route path="/" element={<Navigate to={"/login"} />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
---
+++ frontend/src/constants/index.ts
@@ -0,0 +1,3 @@
+export const DEFAULT_QUERY_OPTIONS: any = {
+  retry: false,
+};
