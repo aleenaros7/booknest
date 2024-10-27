2d80e9d2c55db66d5cc7e177920cd1f5cacda109
Aleena <aleenaros999@gmail.com>
2024-10-27 11:14:22 +0530

    error page styled

--- frontend/src/pages/Error.tsx
+++ frontend/src/pages/Error.tsx
@@ -1,3 +1,17 @@
+import { Box, Typography } from "@mui/material";
+
 export const Error = () => {
-  return <div>404 Not Found</div>;
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        minHeight: "80vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Typography>404 Not Found</Typography>
+    </Box>
+  );
 };
